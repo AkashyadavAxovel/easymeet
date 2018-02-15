2b62fa2c7cb92af1280acafaa44cb362676a2582
deepakdwive <jitender.axovel@gmail.com>
2018-02-15 12:57:09 -0500

    Added new routes for event management

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ Route::get('/', function () {
 Route::get('login', 'LoginController@index');
 Route::post('login', 'LoginController@login');
 Route::get('logout', 'LoginController@logout');
-Route::get('dashboard', function () {
-    return view('admin.dashboard.home');
-});
+Route::get('dashboard', 'AdminDashboard@index');
 Route::resource('users','UserController');
 Route::get('users/block/{id}','UserController@block');
+Route::resource('events','EventController');
+Route::get('events/block/{id}','EventController@block');
