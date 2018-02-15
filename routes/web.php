5abfaa88a41b4471efc67579485c4bfef71bcd55
deepakdwive <jitender.axovel@gmail.com>
2018-02-15 11:54:47 -0500

    Create new routes

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+	return redirect('login');
+});
 Route::get('login', 'LoginController@index');
 Route::post('login', 'LoginController@login');
 Route::get('logout', 'LoginController@logout');
 Route::get('dashboard', function () {
     return view('admin.dashboard.home');
 });
+Route::resource('users','UserController');
+Route::get('users/block/{id}','UserController@block');
